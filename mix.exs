7ead6d5bfc053ffa178e3748801205d13e6698fb
diningyo <diningyo.kpuku.jougeki@gmail.com>
2021-01-25 23:29:23 +0900

    added esdl2.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule DmgEmulator.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:esdl2, github: "ninenines/esdl2", branch: "master"}
     ]
   end
 end
